97dfb18aefdd634c6f7a29e54183c9bdd8b83e74
Qs-F <qsf@de-liker.com>
2025-03-06 23:59:44 +0900

    Update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,36 +7,44 @@ export default {
       full: '99999px',
     },
     fontFamily: {
-      'sans': ['system-ui', 'sans-serif'],
+      'sans': ['Open Sans', 'Noto Sans JP', 'sans-serif'],
     },
     fontSize: {
       'medium': [
-        '1rem', // 16px
+        '1.0rem', // 16px
         {
-          lineHeight: '2', // 32px
-          letterSpacing: '0.01em',
-          fontWeight: '500',
+          lineHeight: '2.25rem', // 32px
+          letterSpacing: '0.02em',
+          fontWeight: '450',
         },
       ],
       'large': [
+        '1.25rem', // 24px
+        {
+          lineHeight: '2.5', // 48px
+          letterSpacing: '0.1em',
+          fontWeight: '500',
+        },
+      ],
+      'xlarge': [
         '1.5rem', // 24px
         {
-          lineHeight: '2', // 48px
-          letterSpacing: '0.01em',
+          lineHeight: '2.5', // 48px
+          letterSpacing: '0.05em',
           fontWeight: '500',
         },
       ],
       'huge': [
-        '2rem', // 32px
+        '2.5rem', // 32px
         {
-          lineHeight: '2', // 64px
-          letterSpacing: '0.01em',
-          fontWeight: '500',
+          lineHeight: '4.5rem',
+          letterSpacing: '0.11em',
+          fontWeight: '450',
         },
       ],
     },
     maxWidth: {
-      content: '800px'
+      content: '50rem'
     },
 	},
 	plugins: [],
