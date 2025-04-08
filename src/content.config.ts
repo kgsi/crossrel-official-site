35e6aaa3d96c09b55a99a3318a8e793dc22b0819
kgsi <kuusai1998@gmail.com>
2025-04-09 07:06:04 +0900

    イベントコレクションのターゲットに「すべて」を追加

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,27 +2,29 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const events = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/content/events" }),
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    date: z.date(),
-    target: z.union([z.literal('designer'), z.literal('engineer')]),
-    eventSiteURL: z.string(),
-    thumbnail: image(),
-  })
+  loader: glob({ pattern: '**/*.md', base: './src/content/events' }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      target: z.union([z.literal('designer'), z.literal('engineer'), z.literal('all')]),
+      eventSiteURL: z.string(),
+      thumbnail: image(),
+    }),
 });
 
 const staff = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/content/staff" }),
-  schema: ({ image }) => z.object({
-    name: z.string(),
-    number: z.number(),
-    description: z.string().nullable(),
-    role: z.string().nullable(),
-    x: z.string(),
-    facebook: z.string().optional(),
-    profile: image(),
-  }),
+  loader: glob({ pattern: '**/*.md', base: './src/content/staff' }),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      number: z.number(),
+      description: z.string().nullable(),
+      role: z.string().nullable(),
+      x: z.string(),
+      facebook: z.string().optional(),
+      profile: image(),
+    }),
 });
 
 export const collections = { events, staff };
