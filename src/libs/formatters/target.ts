25977a4f1595e3cd33ea33c1c08742546a91ef5c
kgsi <kuusai1998@gmail.com>
2025-04-09 07:05:46 +0900

    ターゲットフォーマッターに新しいターゲット「すべて」を追加し、コードの整形を改善

--- src/libs/formatters/target.ts
+++ src/libs/formatters/target.ts
@@ -1,6 +1,8 @@
 const targets = {
-  'designer': 'デザイナー',
-  'engineer': 'エンジニア',
-} as const
+  designer: "デザイナー",
+  engineer: "エンジニア",
+  all: "すべて",
+} as const;
 
-export const targetFormatter = (target: keyof typeof targets) => targets[target]
+export const targetFormatter = (target: keyof typeof targets) =>
+  targets[target];
