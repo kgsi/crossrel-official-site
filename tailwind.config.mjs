1d3afef4c61e8054d0a7add6566f9bfe9a69abfc
kgsi <kuusai1998@gmail.com>
2025-04-12 18:32:49 +0900

    修正

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,50 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
     borderRadius: {
       medium: '8px',
       full: '99999px',
     },
     fontFamily: {
-      'sans': ['IBM Plex Sans', 'system-ui', 'sans-serif'],
+      sans: ['IBM Plex Sans', 'system-ui', 'sans-serif'],
     },
     lineHeight: {
-      'default': '1,8',
-      'tight': '1.5',
-      'tighter': '1.2',
-      'none': '1',
+      default: '1,8',
+      tight: '1.5',
+      tighter: '1.2',
+      none: '1',
     },
     fontSize: {
-      's': [
+      s: [
         'var(--font-size-s)',
         {
-          lineHeight: '1.8',
+          lineHeight: '1.5',
           letterSpacing: '0',
         },
       ],
-      'm': [
+      m: [
         'var(--font-size-m)',
         {
-          lineHeight: '1.8',
+          lineHeight: '1.6',
           letterSpacing: '0',
         },
       ],
-      'l': [
+      l: [
         'var(--font-size-l)',
         {
           lineHeight: '1.8',
           letterSpacing: '0',
         },
       ],
-      'xl': [
+      xl: [
         'var(--font-size-xl)',
         {
           lineHeight: '1.8',
           letterSpacing: '0',
         },
       ],
-      'xxl': [
+      xxl: [
         'var(--font-size-xxl)',
         {
           lineHeight: '1.8',
@@ -52,6 +52,6 @@ export default {
         },
       ],
     },
-	},
-	plugins: [],
-}
+  },
+  plugins: [],
+};
