a0809dd6a87a71c75d6a18ea6243370c4c62f550
kgsi <kuusai1998@gmail.com>
2025-04-14 07:27:32 +0900

    スタッフ更新

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,4 @@
 import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
 
 const events = defineCollection({
   type: 'content',
---
+++ src/content/staff/kanbo0605/index.md
@@ -0,0 +1,9 @@
+---
+name: カンボ
+number: 6
+description: CEO/マネージャー
+role: ''
+x: kanbo0605
+facebook: 'takayuki.suzuki.39'
+profile: ./profile.jpg
+---
