32fbcdc1065a108ff084526f6f002b4d26fb2fb6
Shinichi Kogiso <kuusai1998@gmail.com>
2025-10-05 10:07:48 +0900

    package更新&knotsを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 // @ts-check
 import { defineConfig, passthroughImageService } from 'astro/config';
-
+import partytown from '@astrojs/partytown';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://crossrel.jp/', // ここにデプロイ先のドメインを設定
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push', 'gtag'],
+      },
+    }),
+  ],
   image: {
     service: passthroughImageService(),
   },
